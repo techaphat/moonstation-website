a4000e30405b852e14cfcee668433e8a62cfdc2a
techaphat <techaphat@localhost.local>
2025-05-10 20:54:51 +0200

    Design footer

---
+++ src/data/NavLinks.js
@@ -0,0 +1,8 @@
+const NavLinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/" },
+    { name: "Team", path: "/" },
+    { name: "Impressum", path: "/" }
+];
+
+export default NavLinks;
